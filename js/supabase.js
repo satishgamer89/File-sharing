e4426ee6430b2396dd9ed559d5199d00ac907e8f
satishgamer89 <152015658+satishgamer89@users.noreply.github.com>
2026-06-18 12:54:30 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,14 @@
+import { createClient }
+from 'https://esm.sh/@supabase/supabase-js@2';
 
+const supabaseUrl =
+'https://imtjsxymsbeezpggdvpd.supabase.co';
+
+const supabaseKey =
+'sb_publishable_wEKrcNR0Et7FixZIBMJg4Q_tiSIl4C1';
+
+export const supabase =
+createClient(
+    supabaseUrl,
+    supabaseKey
+);
