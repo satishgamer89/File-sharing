7880ca4a3b1bb265b51f8a853198bd28025cacaa
satishgamer89 <152015658+satishgamer89@users.noreply.github.com>
2026-07-18 17:01:04 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -338,6 +338,23 @@ div.remove();
 document
   .getElementById("uploadBtn")
   .addEventListener("click", async () => {
+    
+    
+function formatFileSize(bytes){
+
+if(!bytes) return "0 KB";
+
+const sizes = ["Bytes","KB","MB","GB"];
+
+const i = Math.floor(Math.log(bytes) / Math.log(1024));
+
+return (
+(bytes / Math.pow(1024,i)).toFixed(2) +
+" " +
+sizes[i]
+);
+
+}
     // --------------------
 // Search Documents
 // --------------------
