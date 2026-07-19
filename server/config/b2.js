2b0627a85cc08cfe6a5018cdb637c924eed7faff
satishgamer89 <152015658+satishgamer89@users.noreply.github.com>
2026-07-19 14:25:36 +0530

    Create b2.js

---
+++ server/config/b2.js
@@ -0,0 +1,8 @@
+const B2 = require("backblaze-b2");
+
+const b2 = new B2({
+  applicationKeyId: process.env.B2_KEY_ID,
+  applicationKey: process.env.B2_APPLICATION_KEY,
+});
+
+module.exports = b2;
